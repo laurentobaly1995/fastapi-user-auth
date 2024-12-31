80782ce732303796dec72fb72942009978059cb8
laurentobaly <laurentobaly@gmail.com>
2024-12-31 10:40:48 +0200

    sucsses token

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = Column(String, unique=True, nullable=False)
-    password_hash = Column(String, nullable=False)
+    password_hash = Column(String(60), nullable=False)
     first_name = Column(String)
     last_name = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
